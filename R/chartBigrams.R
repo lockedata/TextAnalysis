d8a49bd6e2f3d4168141f03e511afb3fd2e74659
Steph Locke <steph@itsalocke.com>
2017-04-04 23:49:23 +0100

    simpler chart

--- R/chartBigrams.R
+++ R/chartBigrams.R
@@ -5,17 +5,14 @@
 #' @export
 #'
 #' @examples
-#' df<-data.frame(word1=LETTERS[1:5], word2=LETTERS[2:6])
+#' df<-data.frame(word1=LETTERS[1:5], word2=LETTERS[2:6], n=1:5)
 #' chartBigrams(df)
 #'
-chartBigrams <- function(bigrams) {
-  a <- grid::arrow(type = "closed")
-
-  bigrams %>%
-    igraph::graph_from_data_frame() %>%
-    ggraph::ggraph(layout="fr") +
-    ggraph::geom_edge_link(ggplot2::aes(edge_alpha = n), show.legend = FALSE, arrow = a) +
-    ggraph::geom_node_point(color = "#2165B6", size = 5) +
-    ggraph::geom_node_text(ggplot2::aes(label = name), vjust = 1, hjust = 1) +
+chartConnections <- function(bigrams) {
+  igraph::graph_from_data_frame(bigrams) %>%
+    ggraph::ggraph(layout = "fr") +
+    ggraph::geom_edge_link() +
+    ggraph::geom_node_point() +
+    ggraph::geom_node_text(ggplot2::aes(label = name), vjust = 1, hjust = 1)+
     ggplot2::theme_void()
 }
