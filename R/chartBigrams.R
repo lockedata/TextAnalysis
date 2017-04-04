254410b64337c88cd8085ea50e503fe5e97e1e4b
Steph Locke <steph@itsalocke.com>
2017-04-04 23:19:40 +0100

    chart

--- R/chartBigrams.R
+++ R/chartBigrams.R
@@ -13,7 +13,7 @@ chartBigrams <- function(bigrams) {
 
   bigrams %>%
     igraph::graph_from_data_frame() %>%
-    ggraph::ggraph() +
+    ggraph::ggraph(layout="fr") +
     ggraph::geom_edge_link(ggplot2::aes(edge_alpha = n), show.legend = FALSE, arrow = a) +
     ggraph::geom_node_point(color = "#2165B6", size = 5) +
     ggraph::geom_node_text(ggplot2::aes(label = name), vjust = 1, hjust = 1) +
